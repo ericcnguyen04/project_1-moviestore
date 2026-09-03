bcd135aa6193196cc159bba836e85ba3094a44bf
ericcnguyen04 <ericc.win@gmail.com>
2026-09-03 13:14:07 -0400

    installed pillow

--- moviestore/movies/urls.py
+++ moviestore/movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='movies.index'),
-    path('<int:id>', views.show, name='movies.show')
+    path('<int:id>/', views.show, name='movies.show')
 ] 
+
